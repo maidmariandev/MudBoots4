3334c7be1d2f2bc27299732bf55f56cbc66f8109
runacode <59107019+runacode@users.noreply.github.com>
2021-03-08 12:54:02 -0600

    Tested single Shout command

--- src/test/kotlin/ShoutCommandTests.kt
+++ src/test/kotlin/ShoutCommandTests.kt
@@ -2,22 +2,30 @@ import org.junit.Test
 import sample.Identity
 import sample.Player
 import kotlin.test.assertEquals
-import kotlin.test.asserter
 
-class ShoutCommandTests{
+class ShoutCommandTests {
     @Test
-    fun testSingleTick() {
-        val wrld = World()
-        val t  = TickHandler(wrld);
-
-        wrld.add_player_to_world(Player(Identity(0)))
-        wrld.add_player_to_world(Player(Identity(1, 1)))
-
-       val executedCommands =  t.tick(listOf(PlayerMessage(PlayerCommandType.Shout, 0, 0)))
+    fun testSingleShout() {
+        val world = World()
+        val t = TickHandler(world);
+        world.add_player_to_world(Player(Identity(0)))
+        world.add_player_to_world(Player(Identity(1, 1)))
+        val executedCommands = t.tick(listOf(PlayerMessage(PlayerCommandType.Shout, 0, 0)))
         val cmd = executedCommands[0];
         val bcast = cmd.broadCasts[0];
-
-        //  wrld.tick()
-
+        assertEquals("You hear a shout from the south", bcast.message)
+        assertEquals(1, bcast.player.ident._id)
+    }
+    @Test
+    fun testMultiShout() {
+        val world = World()
+        val t = TickHandler(world);
+        world.add_player_to_world(Player(Identity(0)))
+        world.add_player_to_world(Player(Identity(1, 1)))
+        val executedCommands = t.tick(listOf(PlayerMessage(PlayerCommandType.Shout, 0, 0)))
+        val cmd = executedCommands[0];
+        val bcast = cmd.broadCasts[0];
+        assertEquals("You hear a shout from the south", bcast.message)
+        assertEquals(1, bcast.player.ident._id)
     }
 }
